6dea05be6b18b8f33ff07b74663ec904c63c3d15
Blacknapoleon99 <127541113+Blacknapoleon99@users.noreply.github.com>
2024-01-16 04:13:20 +0100

    Added delete conflicts method

--- src/main/java/com/example/_users_api/controllers/ConflictsController.java
+++ src/main/java/com/example/_users_api/controllers/ConflictsController.java
@@ -1,13 +1,21 @@
 package com.example._users_api.controllers;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ConflictsController {
+
+
     @GetMapping("/conflict")
     public String getConflict(){
         return "Getting conflicts";
+    }
+
+    @DeleteMapping("/conflict")
+    public String deleteConflict(){
+        return "Deleting conflicts";
     }
 
 }
