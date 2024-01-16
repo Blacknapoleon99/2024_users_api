c701120a7e7d44e94ee5fb18faab9dd787cf3e86
Blacknapoleon99 <127541113+Blacknapoleon99@users.noreply.github.com>
2024-01-16 03:02:03 +0100

    Added TestController

--- src/main/java/com/example/_users_api/controllers/TestController.java
+++ src/main/java/com/example/_users_api/controllers/TestController.java
@@ -1,2 +1,15 @@
-package com.example._users_api.controllers;public class TestController {
+package com.example._users_api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String getTest(){
+        return "A test of the Controller that we are using for a seperate branch";
+    }
+
+
 }
